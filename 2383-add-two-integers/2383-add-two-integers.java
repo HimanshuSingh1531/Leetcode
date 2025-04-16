f10c555babe5144eae6a2bbb109df9204f423667
HimanshuSingh <himanshus85549@gmail.com>
2025-04-16 19:44:45 +0530

    Time: 8 ms (0.52%), Space: 41.4 MB (7.26%) - LeetHub

---
+++ 2383-add-two-integers/2383-add-two-integers.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int sum(int num1, int num2) {
+        int n1 = num1;
+        int n2 = num2;
+        System.out.println("num1 is : " + n1);
+        System.out.println("num2 is : " + n2);
+        return n1+n2;
+    }
+}
